6fc61f28163e63454d4e89eb00b9957287d31118
wangtianrui <425281019@qq.com>
2023-09-06 15:11:55 +0800

    更新BigDecimal无法接到null的bug

--- tr-common/tr-common-web/src/main/java/io/github/tr/common/web/config/ConverterConfig.java
+++ tr-common/tr-common-web/src/main/java/io/github/tr/common/web/config/ConverterConfig.java
@@ -4,6 +4,7 @@ import io.github.tr.common.web.converter.DateTimeConverter;
 import org.apache.commons.beanutils.BeanUtilsBean;
 import org.apache.commons.beanutils.ConvertUtilsBean;
 import org.apache.commons.beanutils.PropertyUtilsBean;
+import org.apache.commons.beanutils.converters.BigDecimalConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -24,6 +25,9 @@ public class ConverterConfig {
         convertUtilsBean.register(dtConverter, java.sql.Date.class);
         convertUtilsBean.register(dtConverter, LocalDate.class);
         convertUtilsBean.register(dtConverter, LocalDateTime.class);
+        // 这里一定要注册默认值，使用null也可以
+        BigDecimalConverter bd = new BigDecimalConverter(null);
+        convertUtilsBean.register(bd, java.math.BigDecimal.class);
         return new BeanUtilsBean(convertUtilsBean, new PropertyUtilsBean());
     }
 }
