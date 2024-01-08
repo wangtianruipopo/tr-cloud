8ee727e2d84940d642865bc22eb9a514f0a752d8
wangtianrui <425281019@qq.com>
2024-01-08 11:08:06 +0800

    1

--- tr-common/tr-common-mysql/src/main/java/io/github/tr/common/mysql/config/MyBatisPlusConfig.java
+++ tr-common/tr-common-mysql/src/main/java/io/github/tr/common/mysql/config/MyBatisPlusConfig.java
@@ -6,18 +6,28 @@ import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerIntercept
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.annotation.Resource;
+
 /**
  * @author wangtianrui
  */
 @Configuration
 public class MyBatisPlusConfig {
+
+    @Resource
+    private DbType dbType;
+
     /**
      * 新的分页插件,一缓和二缓遵循mybatis的规则,需要设置 MybatisConfiguration#useDeprecatedExecutor = false 避免缓存出现问题
      */
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        DbType defaultType = DbType.MYSQL;
+        if (dbType != null) {
+            defaultType = dbType;
+        }
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
-        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(defaultType));
         return interceptor;
     }
 }
---
+++ tr-core/tr-core-gen/src/main/java/io/github/wangtianruipopo/trcoregen/config/MybatisConfig.java
@@ -0,0 +1,24 @@
+package io.github.wangtianruipopo.trcoregen.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class MybatisConfig {
+
+    @Value("${db.type}")
+    private String type;
+
+    @Bean
+    public DbType dbType() {
+        switch (type) {
+            case "MYSQL": return DbType.MYSQL;
+            case "POSTGRE_SQL": return DbType.POSTGRE_SQL;
+            default: return DbType.MYSQL;
+        }
+    }
+}
