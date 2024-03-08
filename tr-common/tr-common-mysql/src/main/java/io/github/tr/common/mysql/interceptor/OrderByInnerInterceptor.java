27839a5f75c3c4e1890aa7106e0d89e17b8c7098
wangtianrui <425281019@qq.com>
2024-03-08 09:34:25 +0800

    1

--- tr-common/tr-common-mysql/src/main/java/io/github/tr/common/mysql/interceptor/OrderByInnerInterceptor.java
+++ tr-common/tr-common-mysql/src/main/java/io/github/tr/common/mysql/interceptor/OrderByInnerInterceptor.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.core.toolkit.PluginUtils;
 import com.baomidou.mybatisplus.extension.plugins.inner.InnerInterceptor;
 import io.github.tr.common.base.query.OrderByColumn;
 import io.github.tr.common.mysql.constant.MybatisConstant;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.binding.MapperMethod;
 import org.apache.ibatis.executor.Executor;
 import org.apache.ibatis.mapping.BoundSql;
@@ -14,18 +15,20 @@ import org.apache.ibatis.session.RowBounds;
 
 import java.util.List;
 
+@Slf4j
 public class OrderByInnerInterceptor implements InnerInterceptor {
     @Override
     public void beforeQuery(Executor executor, MappedStatement ms, Object parameter, RowBounds rowBounds, ResultHandler resultHandler, BoundSql boundSql) {
         if (SqlCommandType.SELECT.equals(ms.getSqlCommandType())) {
             System.out.println("Intercepted SELECT statement: " + boundSql.getSql());
             if (parameter == null) return;
+            if (!(parameter instanceof MapperMethod.ParamMap)) return;
             MapperMethod.ParamMap<?> p = (MapperMethod.ParamMap<?>) parameter;
-            System.out.println(p);
 
-            if (p.get(MybatisConstant.ORDER_NAME) != null) {
+            if (p.containsKey(MybatisConstant.ORDER_NAME)) {
                 @SuppressWarnings("unchecked")
-                List<OrderByColumn> order = (List<OrderByColumn>) p.get("order");
+                List<OrderByColumn> order = (List<OrderByColumn>) p.get(MybatisConstant.ORDER_NAME);
+                if (order == null || order.isEmpty()) return;
                 StringBuilder sql = new StringBuilder(boundSql.getSql());
                 sql = new StringBuilder("select * from (" + sql + ") as t order by ");
                 for (int i = 0; i < order.size(); i++) {
